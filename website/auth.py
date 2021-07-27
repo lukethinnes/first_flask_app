a7298f3f51ea9d16ed7d967eecd252c0275f7240
Luke Thinnes <lukethinnes@yahoo.com>
2021-07-26 19:48:19 -0600

    Added post and sign up requests.

--- website/auth.py
+++ website/auth.py
@@ -2,14 +2,22 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html', text='Testing') 
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.moeth == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
     return render_template('sign_up.html') 
